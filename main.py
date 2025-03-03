0a79b52b31a849f0e6c9234886fbc3c6b62096f9
latha <lathakh2018@gmail.com>
2025-03-03 18:56:32 +0530

    data validation step done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.networksecurity.components.data_ingestion import DataIngestion
+from src.networksecurity.components.data_validation import DataValidation
 from src.networksecurity.exception.exception import NetWorkSecurityException
 from src.networksecurity.logging.logger import logging
 from src.networksecurity.entity.config_entity import DataIngestionConfig
+from src.networksecurity.entity.config_entity import DataValidationConfig
 from src.networksecurity.entity.config_entity import TrainingPipelineConfig
 import os
 import sys
@@ -10,10 +12,24 @@ import sys
 if __name__ == '__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
+
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
+
         logging.info("initate the data ingestion")
+
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+
+        logging.info("data initiated completed")
         print(dataingestionartifact)
+
+        datavalidationconfig=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
+
+        logging.info("initate the data valdation")
+
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetWorkSecurityException(e,sys) 
--- src/networksecurity/entity/artifact_entity.py
+++ src/networksecurity/entity/artifact_entity.py
@@ -7,4 +7,13 @@ class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
 
-    
+
+# output of data validation
+@dataclass
+class  DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str   
